d841484e604120b8a027b2346cab1fda3a01f9ac
Illia Zolotukha <illia@luday.se>
2023-10-20 11:47:41 +0300

    Set up cors.rb, development.rb, gitignore

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://afd9-91-90-11-229.ngrok-free.app'
+        origins '*'
         resource '*',
             headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head]
+            methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
 end

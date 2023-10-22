324b4f435876b56aa1b9aa3439c6a4245489feda
Illia Zolotukha <illia@luday.se>
2023-10-23 01:37:51 +0300

    UPDATE: cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://001elijah.github.io/rails-e-commerce-store'
+        origins 'https://001elijah.github.io/rails-e-commerce-store/'
 
         resource '*',
             headers: :any,

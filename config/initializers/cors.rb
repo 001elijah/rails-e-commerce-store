3c6cdb7c486bf6b0d340fdfc842c7bd10132124f
Illia Zolotukha <illia@luday.se>
2023-10-23 01:34:27 +0300

    UPDATE: cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,34 +1,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
-        resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
-
-    allow do
-        origins 'http://localhost:5173'
-        resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
-
-    allow do
         origins 'https://001elijah.github.io/rails-e-commerce-store'
-        resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
 
-    allow do
-        origins 'https://rails-e-ecommerce-store.onrender.com'
-        resource '*',
-            headers: :any,
-            methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
-
-    allow do
-        origins 'http://0.0.0.0:10000'
         resource '*',
             headers: :any,
             methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true

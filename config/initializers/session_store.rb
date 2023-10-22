7bb9d0735fae316c982802282e342523785491af
Illia Zolotukha <illia@luday.se>
2023-10-23 02:00:22 +0300

    UPDATE: session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://001elijah.github.io/rails-e-commerce-store"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "001elijah.github.io"
 else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end

d21c81ea4bba17c57cbb2422bb366a4482d10b25
Illia Zolotukha <illia@luday.se>
2023-10-23 11:01:37 +0300

    UPDATE: application.rb session_store.rb

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,6 @@ module RailsECommerceStore
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
-    config.middleware.use ActionDispatch::Cookies
-    config.middleware.use ActionDispatch::Session::CookieStore
 
     # Configuration for the application, engines, and railties goes here.
     #
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,1 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "rails-e-ecommerce-store.onrender.com"
-else
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
-end
+Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "rails-e-ecommerce-store.onrender.com"

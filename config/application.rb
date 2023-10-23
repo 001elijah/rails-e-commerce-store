f22de2d9fd9ba82743b79a0e2148537e5f349325
Illia Zolotukha <illia@luday.se>
2023-10-23 13:14:10 +0300

    UPDATE: application.rb

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,11 @@ module RailsECommerceStore
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # 
+    # Specify cookies SameSite protection level: either :none, :lax, or :strict.
+    # This change is not backwards compatible with earlier Rails versions. 
+    # It's best enabled when your entire app is migrated and stable on 6.1.
+    # Was not in Rails 6.0. Default in rails 6.1 is :lax, not :strict
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end

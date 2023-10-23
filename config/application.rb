93dc5b2d40a48202a100223f6e501e0b5743110d
Illia Zolotukha <illia@luday.se>
2023-10-23 10:46:31 +0300

    UPDATE: application.rb

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RailsECommerceStore
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.middleware.use actiondispatch::cookies
+    config.middleware.use actiondispatch::session::cookiestore
 
     # Configuration for the application, engines, and railties goes here.
     #

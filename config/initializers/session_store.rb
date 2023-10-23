9cd2a6c54b38ed6d7d40bd2d2d3a4bbf7942446b
Illia Zolotukha <illia@luday.se>
2023-10-23 13:02:01 +0300

    UPDATE: session_store.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_rails_e_commerce_store", domain: "rails-e-ecommerce-store.onrender.com"
+    Rails.application.config.session_store :cookie_store, key: "_rails_e_commerce_store", domain: "rails-e-ecommerce-store.onrender.com", secure: Rails.env.production?
 else
     Rails.application.config.session_store :cookie_store, key: "_rails_e_commerce_store"
 end

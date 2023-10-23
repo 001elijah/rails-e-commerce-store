c0120b2afe1faf3e94ac022777a82b6119e765e1
Illia Zolotukha <illia@luday.se>
2023-10-23 09:50:53 +0300

    UPDATE: react_orders_controller.rb

--- app/controllers/react_orders_controller.rb
+++ app/controllers/react_orders_controller.rb
@@ -1,27 +1,25 @@
 class ReactOrdersController < ApplicationController
+    include CurrentUserConcern
+
     def index
         @react_orders = ReactOrder.all
     end
 
     def create
-        if session[:user_id]
-                @current_react_user = ReactUser.find(session[:user_id])
+        @react_order = @current_react_user.react_orders.create!(
+        react_user_id: params["user_id"],
+        amount: params["amount"]
+        )
 
-                @react_order = @current_react_user.react_orders.create!(
-                react_user_id: params["user_id"],
-                amount: params["amount"]
-                )
-
-                if @react_order
-                    render json: {
-                        status: :created,
-                        order: @react_order
-                    }
-                else
-                    render json: {
-                        status: 500
-                    }
-                end
-            end
+        if @react_order
+            render json: {
+                status: :created,
+                order: @react_order
+            }
+        else
+            render json: {
+                status: 500
+            }
         end
     end
+end

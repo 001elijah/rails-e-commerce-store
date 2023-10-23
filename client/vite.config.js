5ae694799d487cac5751291fc18e350f4c983e7d
Illia Zolotukha <illia@luday.se>
2023-10-23 14:41:37 +0300

    FIX: file not found on gh-pages: UPDATE: index.html, vite.config.js; MOVE: 404.html

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      fallback: "/404.html",
+    },
+  },
   plugins: [react()],
-  base: "",
+  base: "/rails-e-commerce-store/",
 });
